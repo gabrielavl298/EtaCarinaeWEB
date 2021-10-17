c08aea788734d55dd777d2fea1fbf1740f14c6c1
gabrielavl298 <57778718+gabrielavl298@users.noreply.github.com>
2021-10-17 17:17:09 -0500

    rutas
    rutas correctas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+//import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
+/* / This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { initializeApp } from "firebase/app";
+//import { initializeApp } from "firebase/app"; */
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-  apiKey: 'AIzaSyCxc4c4cQ2CLqpvGL2z7qiGCE_lbfKZ4RM',
-  authDomain: 'etacarinae-e197e.firebaseapp.com',
-  projectId: 'etacarinae-e197e',
-  storageBucket: 'etacarinae-e197e.appspot.com',
-  messagingSenderId: '605036753784',
-  appId: '1:605036753784:web:6c08c744b3b0e2a75c0e8d'
-},
+    firebaseConfig : {
+    apiKey: 'AIzaSyCxc4c4cQ2CLqpvGL2z7qiGCE_lbfKZ4RM',
+    authDomain: 'etacarinae-e197e.firebaseapp.com',
+    projectId: 'etacarinae-e197e',
+    storageBucket: 'etacarinae-e197e.appspot.com',
+    messagingSenderId: '605036753784',
+    appId: '1:605036753784:web:6c08c744b3b0e2a75c0e8d'
+  }
 
 };
 
